38fef9e178610b25b642d96a0b356ae5dcc6c02c
abdurasulov-olqar <abdurasulovolqar123@gmail.com>
2023-03-09 09:12:01 +0500

    send message

--- sendMessage.py
+++ sendMessage.py
@@ -1,9 +1,37 @@
 import os
+import time
 import requests
 
-TOKEN = os.environ['TOKEN']
 
-def sendMessage(chat_id:str, text:str):
-    URL = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
-    
-    
+
+TOKEN = '5591155154:AAHn0llNadmGCykPsdpkfc5OXcv54S0MQyc'
+URL = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
+
+BASE_URL = f'https://api.telegram.org/bot{TOKEN}/getUpdates'
+
+update_id = -1
+
+while True:
+    response = requests.get(url=BASE_URL)
+    updates=response.json()['result']
+
+    last_update = updates[-1]
+
+    last_update_id = last_update['update_id']
+    if update_id != last_update_id:
+
+        last_msg = last_update['message']
+        chat_id = last_msg['chat']['id']
+        text = last_msg['text']
+       
+       
+        print(chat_id)
+        payload = {
+            "chat_id": chat_id,
+            "text": text
+            }
+        requests.get(url=URL, params=payload)
+
+        update_id = last_update_id
+    time.sleep(1)
+
